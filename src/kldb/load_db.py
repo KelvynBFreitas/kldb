7a94ed8cb60dbe52e79c88f798d68ecd94f68828
Kelvyn Freitas <kelvyn_fr21@hotmail.com>
2025-10-07 11:18:03 -0300

    Senha aceitar caracters especiais

--- src/kldb/load_db.py
+++ src/kldb/load_db.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
+from urllib.parse import quote_plus  # <-- necessário para escapar a senha
 
 class kloracle:
     def __init__(self, env_file=".env", folder="consultas", port=1521):
@@ -18,7 +19,10 @@ class kloracle:
         if not all([user, password, host, service_name]):
             raise ValueError("⚠️ Variáveis de ambiente do Oracle não foram encontradas no .env")
 
-        dsn = f"oracle+oracledb://{user}:{password}@{host}:{port}/?service_name={service_name}"
+        # Escapa caracteres especiais da senha
+        password_esc = quote_plus(password)
+
+        dsn = f"oracle+oracledb://{user}:{password_esc}@{host}:{port}/?service_name={service_name}"
         self.engine = create_engine(dsn, pool_pre_ping=True)
         self.folder = folder
 
@@ -36,6 +40,7 @@ class kloracle:
         with self.engine.connect() as conn:
             df = pd.read_sql(text(query), conn, params=params or {})
         return df
+
 
 
 class klpostgres:
